01a4757fd5a02b20c53cbf01605879ad5b6a090a
Shawn Huang <shawn.huang.y.h@gmail.com>
2019-06-14 17:56:19 +0800

    bug fix
    bug fix

--- amap.py
+++ amap.py
@@ -7,14 +7,14 @@ amap_api_key = ''  # 输入自己的key
 poi_search_url = 'https://restapi.amap.com/v3/place/around?'
 location = '121.459126,31.162659'
 radius = '1500'
-types = '050800'
+types = '120000'
 
 # 设置爬虫网络链接测试链接
 test_url = 'https://www.baidu.com'
 
 # 设置文件输出名
 place = '云景路'
-output_type = '面包店'
+output_type = '商务住宅'
 file_name = place + output_type + '.xls'
 
 # 创建表格
@@ -28,13 +28,15 @@ while page <=100:
     req_url = poi_search_url + 'key=' + str(amap_api_key) + '&location=' + str(location) + '&radius=' + str(radius) + '&types=' + str(types) + '&page=' + str(page)
     while k<= 19:
         try:
+            line = (page - 1) * 20 + k
             result = requests.get(req_url)
             result.raise_for_status()
             content = result.text
             json_dict = json.loads(content)
-            worksheet.write(k, 0, json_dict['pois'][k]['name'])
-            worksheet.write(k, 1, json_dict['pois'][k]['location'])
-            worksheet.write(k, 2, json_dict['pois'][k]['type'])
+            worksheet.write(line, 0, json_dict['pois'][k]['name'])
+            worksheet.write(line, 1, json_dict['pois'][k]['location'])
+            worksheet.write(line, 2, json_dict['pois'][k]['type'])
+            print("数据正在获取中，请耐心等待。")
             k = k + 1
         except:
             try:
